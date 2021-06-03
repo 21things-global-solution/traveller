f7128f3bf0f1437e4a4c1882e13c03cadaf54dbb
ozluuh <ozluuh@gmail.com>
2021-06-03 20:12:19 -0300

    initial commit

---
+++ src/main/java/br/com/traveller/App.java
@@ -0,0 +1,13 @@
+package br.com.traveller;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
