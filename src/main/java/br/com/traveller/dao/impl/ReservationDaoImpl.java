88f1cbb3491bd79804f0df04ffbc05d85117edaf
ozluuh <ozluuh@gmail.com>
2021-06-04 17:14:36 -0300

    feat: add reservation dao implementation

---
+++ src/main/java/br/com/traveller/dao/impl/ReservationDaoImpl.java
@@ -0,0 +1,14 @@
+package br.com.traveller.dao.impl;
+
+import javax.persistence.EntityManager;
+
+import br.com.traveller.dao.ReservationDao;
+import br.com.traveller.model.Reservation;
+
+public class ReservationDaoImpl extends PersistedImpl<Reservation, Long> implements ReservationDao {
+
+    protected ReservationDaoImpl(EntityManager manager) {
+        super(manager);
+    }
+
+}
