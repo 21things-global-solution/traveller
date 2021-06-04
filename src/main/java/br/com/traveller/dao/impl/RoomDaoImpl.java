fd2450732bdadc963f42314fe1a3658dd49448c6
ozluuh <ozluuh@gmail.com>
2021-06-04 17:14:43 -0300

    feat: add room dao implementation

---
+++ src/main/java/br/com/traveller/dao/impl/RoomDaoImpl.java
@@ -0,0 +1,14 @@
+package br.com.traveller.dao.impl;
+
+import javax.persistence.EntityManager;
+
+import br.com.traveller.dao.RoomDao;
+import br.com.traveller.model.Room;
+
+public class RoomDaoImpl extends PersistedImpl<Room, Long> implements RoomDao {
+
+    protected RoomDaoImpl(EntityManager manager) {
+        super(manager);
+    }
+
+}
