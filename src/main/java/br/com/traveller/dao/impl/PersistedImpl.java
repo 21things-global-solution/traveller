4dc094b246149f05ef36d3704569f01347661976
ozluuh <ozluuh@gmail.com>
2021-06-04 16:59:20 -0300

    feat: add generic dao implementation

---
+++ src/main/java/br/com/traveller/dao/impl/PersistedImpl.java
@@ -0,0 +1,58 @@
+package br.com.traveller.dao.impl;
+
+import java.lang.reflect.ParameterizedType;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+
+import br.com.traveller.dao.Persisted;
+import br.com.traveller.exception.TransactionException;
+
+public class PersistedImpl<E, K> implements Persisted<E, K> {
+
+    private EntityManager manager;
+    private Class<E> clazz;
+
+    @SuppressWarnings("unchecked")
+    protected PersistedImpl(EntityManager manager) {
+        this.manager = manager;
+        this.clazz = (Class<E>) ((ParameterizedType) getClass().getGenericSuperclass()).getActualTypeArguments()[0];
+    }
+
+    @Override
+    public void create(E entity) {
+        manager.persist(entity);
+    }
+
+    @Override
+    public void update(E entity) {
+        manager.merge(entity);
+    }
+
+    @Override
+    public E findById(K id) {
+        return manager.find(clazz, id);
+    }
+
+    @Override
+    public void delete(K id) {
+        E entity = findById(id);
+
+        manager.remove(entity);
+    }
+
+    @Override
+    public void commit() throws TransactionException {
+        EntityTransaction trx = null;
+        try {
+            trx = manager.getTransaction();
+            trx.begin();
+            trx.commit();
+        } catch (Exception e) {
+            if (trx != null) {
+                trx.rollback();
+            }
+            throw new TransactionException(e.getMessage());
+        }
+    }
+}
