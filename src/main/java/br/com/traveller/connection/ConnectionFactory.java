355019740260d7fae1851eb86eeb6a6f5a8591c4
ozluuh <ozluuh@gmail.com>
2021-06-04 17:19:21 -0300

    feat: add connection factory

---
+++ src/main/java/br/com/traveller/connection/ConnectionFactory.java
@@ -0,0 +1,22 @@
+package br.com.traveller.connection;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class ConnectionFactory {
+
+    private static final String PERSISTENCE_UNIT = "oraclePu";
+
+    private static EntityManagerFactory factory;
+
+    private ConnectionFactory() {
+    }
+
+    public static EntityManagerFactory getInstance() {
+        if (factory == null) {
+            factory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT);
+        }
+
+        return factory;
+    }
+}
