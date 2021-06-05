7bd903a45aa768b5629392c8fe918f0e9d0d3487
ozluuh <ozluuh@gmail.com>
2021-06-04 23:06:02 -0300

    refactor: add attributes

--- src/main/java/br/com/traveller/model/Reservation.java
+++ src/main/java/br/com/traveller/model/Reservation.java
@@ -1,5 +1,7 @@
 package br.com.traveller.model;
 
+import java.util.Calendar;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -8,6 +10,10 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import org.hibernate.annotations.CreationTimestamp;
 
 @Entity
 @Table(name = "T_GS_RESERVA")
@@ -28,19 +34,31 @@ public class Reservation {
     @JoinColumn(name = "cd_hotel", nullable = false)
     private Hotel hotel;
 
+    @CreationTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "dt_check_in")
+    private Calendar checkIn;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "dt_check_out")
+    private Calendar checkOut;
+
     @Override
     public String toString() {
-        return "Reservation [days=" + days + ", id=" + id + "]";
+        return "Reservation [checkIn=" + checkIn + ", checkOut=" + checkOut + ", customer=" + customer + ", days="
+                + days + ", hotel=" + hotel + ", id=" + id + "]";
     }
 
     public Reservation() {
     }
 
-    public Reservation(Long id, Integer days, Customer customer, Hotel hotel) {
+    public Reservation(Long id, Integer days, Customer customer, Hotel hotel, Calendar checkIn, Calendar checkOut) {
         this.id = id;
         this.days = days;
         this.customer = customer;
         this.hotel = hotel;
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
     }
 
     public Long getId() {
@@ -73,6 +91,22 @@ public class Reservation {
 
     public void setHotel(Hotel hotel) {
         this.hotel = hotel;
+    }
+
+    public Calendar getCheckIn() {
+        return checkIn;
+    }
+
+    public void setCheckIn(Calendar checkIn) {
+        this.checkIn = checkIn;
+    }
+
+    public Calendar getCheckOut() {
+        return checkOut;
+    }
+
+    public void setCheckOut(Calendar checkOut) {
+        this.checkOut = checkOut;
     }
 
 }
