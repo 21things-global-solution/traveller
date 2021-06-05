ab7f23f831c75fbc0cc661de5df9aac1dc5e9138
ozluuh <ozluuh@gmail.com>
2021-06-05 01:44:02 -0300

    refactor: add relation between address and zipcoe

--- src/main/java/br/com/traveller/model/Address.java
+++ src/main/java/br/com/traveller/model/Address.java
@@ -1,11 +1,13 @@
 package br.com.traveller.model;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
@@ -32,6 +34,7 @@ public class Address {
 
     @Id
     @JoinColumn(name = "cd_logradouro", nullable = false)
+    @OneToOne(cascade = CascadeType.ALL)
     private ZipCode zipCode;
 
     @Override
