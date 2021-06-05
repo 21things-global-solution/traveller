81dda93233c6b0cc21c8fda7bbc3d784770a8bdb
ozluuh <ozluuh@gmail.com>
2021-06-05 19:40:31 -0300

    refactor: increment connection with type

--- src/main/java/br/com/traveller/connection/ConnectionFactory.java
+++ src/main/java/br/com/traveller/connection/ConnectionFactory.java
@@ -1,20 +1,24 @@
 package br.com.traveller.connection;
 
+import static br.com.traveller.connection.ConnectionType.QUERY;
+
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 public class ConnectionFactory {
 
-    private static final String PERSISTENCE_UNIT = "oraclePU";
+    private static final String PERSISTENCE_UNIT_DML = "dml-persistence-unit";
+    private static final String PERSISTENCE_UNIT_DQL = "dql-persistence-unit";
 
     private static EntityManagerFactory factory;
 
     private ConnectionFactory() {
     }
 
-    public static EntityManagerFactory getInstance() {
+    public static EntityManagerFactory getInstance(ConnectionType type) {
         if (factory == null) {
-            factory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT);
+            boolean isQuery = type.equals(QUERY);
+            factory = Persistence.createEntityManagerFactory(isQuery ? PERSISTENCE_UNIT_DQL : PERSISTENCE_UNIT_DML);
         }
 
         return factory;
---
+++ src/main/java/br/com/traveller/connection/ConnectionType.java
@@ -0,0 +1,5 @@
+package br.com.traveller.connection;
+
+public enum ConnectionType {
+    MANIPULATE, QUERY
+}
