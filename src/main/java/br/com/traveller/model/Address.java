34ef9edb35e68225ed09641e908c47f24bfc6f11
ozluuh <ozluuh@gmail.com>
2021-06-05 02:13:14 -0300

    fix: correct relation

--- src/main/java/br/com/traveller/model/Address.java
+++ src/main/java/br/com/traveller/model/Address.java
@@ -7,6 +7,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
@@ -34,7 +35,7 @@ public class Address {
 
     @Id
     @JoinColumn(name = "cd_logradouro", nullable = false)
-    @OneToOne(cascade = CascadeType.ALL)
+    @ManyToOne(cascade = CascadeType.ALL)
     private ZipCode zipCode;
 
     @Override
