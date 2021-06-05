e3f28b47e407ad8e1175ef7fa97cd5f09f54c3ab
ozluuh <ozluuh@gmail.com>
2021-06-05 19:50:48 -0300

    feat: add  view

--- src/main/java/br/com/traveller/App.java
+++ src/main/java/br/com/traveller/App.java
@@ -1,13 +1,41 @@
 package br.com.traveller;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.util.List;
+
+import javax.persistence.EntityManager;
+
+import br.com.traveller.connection.ConnectionFactory;
+import br.com.traveller.connection.ConnectionType;
+import br.com.traveller.dao.PhoneDao;
+import br.com.traveller.dao.impl.PhoneDaoImpl;
+import br.com.traveller.model.Phone;
+
+public class App {
+    public static void main(String[] args) {
+        EntityManager em = ConnectionFactory.getInstance(ConnectionType.QUERY).createEntityManager();
+
+        PhoneDao daoPhone = new PhoneDaoImpl(em);
+
+        // Encontrar todos os telefones por parte do nome de cliente
+        System.out.println("\n\nEncontrar todos os telefones por parte do nome de cliente");
+        String header = String.format("| %-50s", "Nome") + String.format("| %-15s ", "Fone")
+                + String.format("| %-9s |", "Tipo");
+        System.out.println(header);
+
+        List<Phone> phones = daoPhone.findByCustomerNameContainingIgnoreCase("eMIlly");
+
+        for (Phone phone : phones) {
+
+            String name = String.format("| %-50s", phone.getCustomer().getName());
+            String number = String.format("| %-15s ",
+                    phone.getNumber().replaceAll("^([0-9]{2})([0-9]{4,5})([0-9]{4})$", "($1) $2-$3"));
+            String type = String.format("| %-9s |", phone.getType().toString());
+
+            System.out.println(name + number + type);
+        }
+
+        // close
+        em.close();
+        ConnectionFactory.getInstance(ConnectionType.QUERY).close();
     }
 }
