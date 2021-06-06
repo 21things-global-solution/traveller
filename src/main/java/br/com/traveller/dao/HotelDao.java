53c98019548c80232438ac9db177406883e2817c
ozluuh <ozluuh@gmail.com>
2021-06-06 14:58:33 -0300

    feat: add method in hotel dao
    now count all room by types and hotel name

--- src/main/java/br/com/traveller/dao/HotelDao.java
+++ src/main/java/br/com/traveller/dao/HotelDao.java
@@ -1,6 +1,16 @@
 package br.com.traveller.dao;
 
 import br.com.traveller.model.Hotel;
+import br.com.traveller.model.RoomType;
 
 public interface HotelDao extends Persisted<Hotel, Long> {
+
+    /**
+     * Count all instances find in database by {@link RoomType} and hotel name
+     * 
+     * @param name hotel name
+     * @param type instance to find
+     * @return Long count all instances
+     */
+    Long countAllByHotelNameAndRoomType(String name, RoomType type);
 }
--- src/main/java/br/com/traveller/dao/impl/HotelDaoImpl.java
+++ src/main/java/br/com/traveller/dao/impl/HotelDaoImpl.java
@@ -4,11 +4,21 @@ import javax.persistence.EntityManager;
 
 import br.com.traveller.dao.HotelDao;
 import br.com.traveller.model.Hotel;
+import br.com.traveller.model.RoomType;
 
 public class HotelDaoImpl extends PersistedImpl<Hotel, Long> implements HotelDao {
 
     public HotelDaoImpl(EntityManager manager) {
         super(manager);
+    }
+
+    @Override
+    public Long countAllByHotelNameAndRoomType(String name, RoomType type) {
+        return manager
+                .createQuery("select COUNT(r) from Room r where r.hotel.name = :name and r.type = :type", Long.class)
+                .setParameter("name", name)
+                .setParameter("type", type)
+                .getSingleResult();
     }
 
 }
