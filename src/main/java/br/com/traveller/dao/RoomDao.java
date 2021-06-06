1269c91743c898a3725bc9983d58bac1fd009f40
ozluuh <ozluuh@gmail.com>
2021-06-06 03:07:13 -0300

    feat: add method

--- src/main/java/br/com/traveller/dao/RoomDao.java
+++ src/main/java/br/com/traveller/dao/RoomDao.java
@@ -1,6 +1,17 @@
 package br.com.traveller.dao;
 
+import java.util.List;
+
 import br.com.traveller.model.Room;
 
 public interface RoomDao extends Persisted<Room, Long> {
+
+    /**
+     * Find all rooms with value less than informed
+     * 
+     * @param value max value to search
+     * 
+     * @return List<Room> all instances
+     */
+    List<Room> findByValueLessThan(Double value);
 }
--- src/main/java/br/com/traveller/dao/impl/RoomDaoImpl.java
+++ src/main/java/br/com/traveller/dao/impl/RoomDaoImpl.java
@@ -1,5 +1,7 @@
 package br.com.traveller.dao.impl;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 
 import br.com.traveller.dao.RoomDao;
@@ -9,6 +11,14 @@ public class RoomDaoImpl extends PersistedImpl<Room, Long> implements RoomDao {
 
     public RoomDaoImpl(EntityManager manager) {
         super(manager);
+    }
+
+    @Override
+    public List<Room> findByValueLessThan(Double value) {
+        return manager
+                .createQuery("from Room r where r.value < :value", Room.class)
+                .setParameter("value", value)
+                .getResultList();
     }
 
 }
